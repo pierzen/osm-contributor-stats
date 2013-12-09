e7dba66adf9001e7e9382496a658f89169aa612f
Pierre Beland <infosbelas-am@yahoo.fr>
2013-12-09 15:16:24 +0000

    Update Script-to-extract-Extract-Objects-Calculate-Statistics-from-OsmContributorStats-Module.py

--- Script-to-extract-Extract-Objects-Calculate-Statistics-from-OsmContributorStats-Module.py
+++ Script-to-extract-Extract-Objects-Calculate-Statistics-from-OsmContributorStats-Module.py
@@ -6,7 +6,7 @@
 # https://github.com/pierzen/osm-contributor-stats/blob/master/Script-to-run-OsmContributorStats-Module-Extract-Objects-Calculate-Statistics.py
 # Pierre Beland, 10-2013
 # Example running version 0.1 of OsmContributorStats
-# OSM Contribitors Histor Statistics, for a specific bbox zone and date range
+# OSM Contributors Histor Statistics, for a specific bbox zone and date range
 # STATISTIQUES Historiques, contributeurs OSM pour une zone bbox et paire de dates
 #========================================================================="
 #========================================================================="
@@ -36,16 +36,17 @@ users[1] = ["zyx","avb Yul"]
 
 
 # Example - Lome, Togo
-
+# The examples below defines a Bbox covering Lome, Togo. The period covered si from 2013-06-26 to 2013-06-27.
+  
 # Step 1 - Extract History Data  - Extraire les données historiques
 ContributorStats.API6_Collect_Changesets(team_from=0,team_to=0,from_date="2013-06-26",
-    to_date="2013-08-14",
+    to_date="2013-06-27",
     min_lon=1.151,max_lon=1.2888,min_lat=6.1288,max_lat=6.2375,
     prefix="osmef-togo-",users=users)
 
 # Step 2 - Statistics from data stored locally	- Statistiques produite à partir des données enregistrées localement
 ContributorStats.Changesets_Contributor_Statistics(team_from=0,team_to=0,from_date="2013-06-26",
-    to_date="2013-06-28",
+    to_date="2013-06-27",
     min_lon=1.151,max_lon=1.2888,min_lat=6.1288,max_lat=6.2375,
     prefix="osmef-togo-",users=users)
 
