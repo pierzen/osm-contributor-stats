cbd517b94e8075e2c45166f0cdc6988d485ba266
RK Aranas <rk.aranas@gmail.com>
2018-09-22 16:00:30 +0800

    Fix indentation error

--- Script-to-extract-Extract-Objects-Calculate-Statistics-from-OsmContributorStats-Module.py
+++ Script-to-extract-Extract-Objects-Calculate-Statistics-from-OsmContributorStats-Module.py
@@ -13,18 +13,19 @@
 """
 import os
 # replace below with the directory where both OsmApi.py and OsmContributorStats.py are stored
-os.chdir('c:\OsmContributorStats\\')
+os.chdir('OsmContributorStats')
 import OsmApi
 # Instantiation classe OsmApi
 osmApi = OsmApi.OsmApi(debug=False)
 import OsmContributorStats
 # Instantiation classe OsmContributorStats
-ContributorStats = OsmContributorStats.OsmContributorStats(rep='c:\OsmContributorStats\\',lang="en",debug=False)
+ContributorStats = OsmContributorStats.OsmContributorStats(
+    rep='.', lang="en", debug=False)
 dir(ContributorStats)
 
 #===============================================================================
 # users :  array of contributor ID's or Name by team - if no users, all users in the bbox will be selected
-users=[None]*2
+users = [None] * 2
 users[0] = [""]
 users[1] = [""]
 
@@ -36,21 +37,20 @@ users[1] = ["zyx","avb Yul"]
 """
 
 
-
 # Example - Lome, Togo
 # The examples below defines a Bbox covering Lome, Togo. The period covered si from 2013-06-26 to 2013-06-27.
-  
+
 # Step 1 - Extract History Data  - Extraire les données historiques
-ContributorStats.API6_Collect_Changesets(team_from=0,team_to=0,from_date="2013-06-26",
-    to_date="2013-06-27",
-    min_lon=1.151,max_lon=1.2888,min_lat=6.1288,max_lat=6.2375,
-    prefix="osmef-togo-",users=users)
+ContributorStats.API6_Collect_Changesets(team_from=0, team_to=0, from_date="2013-06-26",
+                                         to_date="2013-06-27",
+                                         min_lon=1.151, max_lon=1.2888, min_lat=6.1288, max_lat=6.2375,
+                                         prefix="osmef-togo-", users=users)
 
 # Step 2 - Statistics from data stored locally	- Statistiques produite à partir des données enregistrées localement
-ContributorStats.Changesets_Contributor_Statistics(team_from=0,team_to=0,from_date="2013-06-26",
-    to_date="2013-06-27",
-    min_lon=1.151,max_lon=1.2888,min_lat=6.1288,max_lat=6.2375,
-    prefix="osmef-togo-",users=users)
+ContributorStats.Changesets_Contributor_Statistics(team_from=0, team_to=0, from_date="2013-06-26",
+                                                   to_date="2013-06-27",
+                                                   min_lon=1.151, max_lon=1.2888, min_lat=6.1288, max_lat=6.2375,
+                                                   prefix="osmef-togo-", users=users)
 
 
 print "\n-----------------------------------------------------"
